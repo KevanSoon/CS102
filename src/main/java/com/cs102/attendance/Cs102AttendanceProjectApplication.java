8186aef093ceb506dbbdc9daf802d53b94b41fc3
minnong <ongminn@gmail.com>
2025-09-16 14:12:02 +0800

    first commit

---
+++ src/main/java/com/cs102/attendance/Cs102AttendanceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cs102.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs102AttendanceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs102AttendanceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs102/attendance/Cs102AttendanceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs102.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs102AttendanceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

249c2648442b3c587aae539f9c63ba3dbeaf9fb6
KevanSoon <64390917+KevanSoon@users.noreply.github.com>
2025-10-13 16:05:31 +0800

    Update DatabaseConfig.java

--- src/main/java/com/cs102/attendance/config/DatabaseConfig.java
+++ src/main/java/com/cs102/attendance/config/DatabaseConfig.java
@@ -4,29 +4,22 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+
 import com.zaxxer.hikari.HikariDataSource;
-import jakarta.annotation.PreDestroy;
 
 @Configuration
 public class DatabaseConfig {
-
     @Autowired
     private DataSource dataSource;
 
-    @PreDestroy
-    public void cleanup() {
-        if (dataSource instanceof HikariDataSource) {
-            ((HikariDataSource) dataSource).close();
+    @EventListener(ContextClosedEvent.class)
+    public void cleanupDataSource(ContextClosedEvent event) {
+        if (dataSource instanceof HikariDataSource hikariDataSource) {
+            System.out.println("Closing HikariCP connection pool...");
+            hikariDataSource.close();
             System.out.println("HikariCP connection pool has been closed");
         }
-    }
-
-    @Bean
-    public javax.sql.DataSource dataSource() {
-        HikariDataSource ds = new HikariDataSource();
-        // Configuration will be picked up from application.yml
-        ds.setRegisterMbeans(true); // Enable JMX monitoring
-        return ds;
     }
 }

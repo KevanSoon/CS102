2962b17ef5fdaeb1bc9687caada24c5d17e0f6d6
KevanSoon <64390917+KevanSoon@users.noreply.github.com>
2025-10-24 14:57:14 +0800

    <refactor> Removed repository folder

--- src/main/java/com/cs102/attendance/repository/AttendanceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs102.attendance.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttendanceRepository /*extends JpaRepository<AttendanceRecord, UUID>*/ {
-    //Optional<AttendanceRecord> findBySessionAndStudent(Session session, Student student);
-    //long countBySessionAndStatus(Session session, Status status);
-} 

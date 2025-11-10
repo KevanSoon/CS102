ff08926aa7428caaffbf4a9aec4eafe029705dea
KevanSoon <64390917+KevanSoon@users.noreply.github.com>
2025-11-11 00:32:45 +0800

    <chore> files cleanup

--- python-face-verification/face-verify.py
+++
@@ -1,8 +0,0 @@
-from deepface import DeepFace
-
-
-result = DeepFace.verify(
-    img1_path = r"C:/PythonFaceVerification/kevan2.jpg",
-    img2_path = r"C:/PythonFaceVerification/kevan.jpg"
-)
-print(result)

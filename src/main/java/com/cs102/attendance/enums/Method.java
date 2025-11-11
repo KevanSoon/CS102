3c0fc68d7380483f28539bcac470662a74c88686
KevanSoon <64390917+KevanSoon@users.noreply.github.com>
2025-11-11 14:39:09 +0800

    <refactor> remove enums folder

--- src/main/java/com/cs102/attendance/enums/Method.java
+++
@@ -1,6 +0,0 @@
-package com.cs102.attendance.enums;
-
-public enum Method {
-    AUTO,
-    MANUAL
-} 
--- src/main/java/com/cs102/attendance/enums/Status.java
+++
@@ -1,7 +0,0 @@
-package com.cs102.attendance.enums;
-
-public enum Status {
-    PRESENT,
-    ABSENT,
-    LATE
-} 
